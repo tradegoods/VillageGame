7050dfa4fd9cb52de7ac067f5855cd99724bbdbe
Kevin Veggeberg <kevinveggeberg@gmail.com>
2015-07-11 14:42:39 -0500

    Added people functionality, need to change the turn sequcne

--- code/Engine.py
+++ code/Engine.py
@@ -1,5 +1,5 @@
 from Buildings import Building
-
+from Professions import Profession
 class Engine(object):
 
     
@@ -7,6 +7,13 @@ class Engine(object):
     
     def __init__(self):
         pass
+        self.productivity = 0
+        self.population = 8
+        self.freePopulation = 8
+        self.completeProjects = []
+        self.availableBuildings = ["House", "Fletchery"]
+        self.currentProjects = []
+        self.availableProfessions = ["Builder", "Farmer"]
 
 
     def month_sequence(self):
@@ -14,11 +21,11 @@ class Engine(object):
             print "You have no current projects"
         else:
             for project in self.currentProjects:
-                project.cost -= 1    
-                if project.cost == 0:
+                project.cost -= self.productivity    
+                if project.cost <= 0:
                     self.completeProjects.append(project)
                 else:
-                    print "%s will be done in %r turns" % (project.name, project.cost)
+                    print "%s will be done in %r turns" % (project.name, (project.cost / self.productivity + (project.cost % self.productivity != 0)))
         print "The following projects are completed:"
         if len(self.completeProjects) == 0:
             print "None"
@@ -41,17 +48,22 @@ class Engine(object):
                 else:
                     project = Building().buildingList[choice]()
                     self.currentProjects.append(project)
-                    print "%s will be done in %r turns" % (project.name, project.cost)
+                    print "%s will be done in %r turns" % (project.name, (project.cost / self.productivity + (project.cost % self.productivity != 0)))
         self.run_month()
 
     def run_month(self):
         self.month_sequence()
 
     def start(self):
-        self.completeProjects = []
-        self.availableBuildings = ["House", "Fletchery"]
-        self.currentProjects = []
-
+        
+        print "Welcome to the Village"
+        print "There are %r people available" % self.population
+        print "Let's assign some tasks"
+        for professionString in self.availableProfessions:
+            print "How many %ss?" % professionString
+            number = int(raw_input("> "))
+            profession = Profession().professionDictionary[professionString]().add_units(self, number)
         self.month_sequence()
+
 game = Engine()
 game.start()
